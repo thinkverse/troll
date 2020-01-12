f6bd91029bddced200689b216f9f9361469b9b4d
thinkverse <ctuvedsson@gmail.com>
2020-01-12 20:39:30 +0100

    (change): Command now supports amount spawned, charged flag and spawning tweaked
    Signed-off-by: thinkverse <ctuvedsson@gmail.com>

--- src/main/java/dev/thinkverse/troll/commands/trolls/CreeperCommand.java
+++ src/main/java/dev/thinkverse/troll/commands/trolls/CreeperCommand.java
@@ -3,7 +3,9 @@ package dev.thinkverse.troll.commands.trolls;
 import dev.thinkverse.troll.TrollPlugin;
 import dev.thinkverse.troll.commands.abstraction.SubCommand;
 import dev.thinkverse.troll.utils.Chat;
+import dev.thinkverse.troll.utils.Teleport;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -49,9 +51,25 @@ public class CreeperCommand extends SubCommand {
       if (!Objects.isNull(target)) {
         if (target.hasPermission("troll.bypass.*") || target.hasPermission("troll.bypass.spawn")) {
           Chat.message(player, String.format("Ooh, seems %s is protected by Herobrine.", target.getName()));
+        } else if (target.isFlying()) {
+          Chat.message(player, String.format("%s cannot be flying.", target.getName()));
+        } else if (target.getLocation().getY() <= 60.0) {
+          Chat.message(player, String.format("%s must be above ground.", target.getName()));
         } else {
-          Creeper creeper = (Creeper) target.getWorld().spawnEntity(target.getLocation(), EntityType.CREEPER);
-          creeper.setTarget(target);
+          final int amount = plugin.getDefaultConfig().getConfig().getInt("troll.creeper.amount", 4);
+          final boolean charged = plugin.getDefaultConfig().getConfig().getBoolean("troll.creeper.charged", false);
+
+          for (int i = 0; i < amount; i++) {
+            try {
+              Location location = Teleport.generateSafeSpawn(target.getLocation(), 5);
+
+              Creeper creeper = (Creeper) target.getWorld().spawnEntity(location, EntityType.CREEPER);
+              creeper.setTarget(target);
+              creeper.setPowered(charged);
+            } catch (StackOverflowError exception) {
+              plugin.getLogger().info(exception.getMessage());
+            }
+          }
 
           Chat.message(target, String.format("&c%s sent you a surprise.", player.getName()));
           Chat.message(player, String.format("&aYou sent %s a friendly creeper.", target.getName()));
